63bedcd24e543f99c1be231f2886a1f3a86d956f
Sigerandi <erandisoublett319@aragon.unam.mx>
2023-11-30 18:02:31 -0600

    Lista tiene estructura completa y botones de eliminar y actualizar

---
+++ lista.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="media/logo.avif" type="image/x-icon" />
+    <meta
+    name="description"
+    content="Pagina de registro de un club de plantas"
+    />
+    <meta name="keywords" content="HTML, Basico, Curso, UNAM, ICO" />
+    <meta name="author" content="Erandi Soublett" />
+    <link rel="stylesheet" href="estilostab.css" />
+    <title>Tabla de usuarios</title>
+</head>
+<body>
+<?php
+    $servidor='';
+    $usuario='';
+    $contra='';
+    $bd='';
+
+    $conexion= new mysqli($servidor,$usuario,$contra, $bd);
+
+    $sql='SELECT * FROM ';
+
+    $regresa= $conexion->query($sql);
+
+    if ($regresa->num_rows > 0){
+        echo '
+<form action="actualizar.php" method="post">
+<div>
+    <label for="actualizar">
+        Actualizar
+    </label>
+    <input type="text">
+</div>
+<button type="submit">Actualizar</button>
+</form>
+<form action="eliminar.php" method="post">
+<div>
+    <label for="eliminar">
+        Eliminar
+    </label>
+    <input type="text">
+</div>
+<button type="submit">Eliminar</button>
+</form>    
+<section>
+    <h1>Registro de usuarios</h1>
+    ';
+
+
+
+    echo'</table>
+    </section>';
+    }
+    else{
+        echo "No hay registros";
+    }
+
+
+</body>
+</html>
+';
+?>
